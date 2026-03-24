481834c6f209790884a1aec9f4071142a8fbc639
John Black <Matrixelectricalservices@gmail.com>
2026-03-24 16:28:40 -0400

    hopefully

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+export const dynamic = "force-dynamic";
 import Link from "next/link";
 import { useParams, useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
